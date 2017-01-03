cd3441206a48da7a379afa8225b0e35d04fa5709
ladydangerdame <turbofemme@gmail.com>
2017-01-03 14:37:25 -0800

    bootstrap, avatars, forms, edit added

--- tipsy-traveller/db/schema.rb
+++ tipsy-traveller/db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103192357) do
+ActiveRecord::Schema.define(version: 20170103221046) do
+
+  create_table "cities", force: :cascade do |t|
+    t.float    "longitude"
+    t.float    "lattitude"
+    t.string   "name"
+    t.text     "description"
+    t.string   "address"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "joins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +47,10 @@ ActiveRecord::Schema.define(version: 20170103192357) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true

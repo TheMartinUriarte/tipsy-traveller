0d498b46ef3bd802bf651c0e3faffd8fe797e729
ladydangerdame <turbofemme@gmail.com>
2017-01-03 15:52:28 -0800

    Preparing to merge

--- tipsy-traveller/db/schema.rb
+++ tipsy-traveller/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103202312) do
+ActiveRecord::Schema.define(version: 20170103221046) do
 
   create_table "cities", force: :cascade do |t|
     t.float    "longitude"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20170103202312) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "text"
+    t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "city_id"

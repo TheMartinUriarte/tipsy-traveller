aa708204bd43995f2a316a30ccbf48a7181f949a
zZissou <kyle.edward.russell@gmail.com>
2017-01-04 11:46:08 -0800

    Added a default user avatar and added the cities to users posts

--- tipsy-traveller/app/models/user.rb
+++ tipsy-traveller/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
-    has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+    has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: 'http://i.imgur.com/2l9mUan.jpg'
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
